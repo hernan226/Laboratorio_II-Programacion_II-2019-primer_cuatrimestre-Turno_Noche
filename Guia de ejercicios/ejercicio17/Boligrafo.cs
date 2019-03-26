2f19b4e50c455e0e9cb30bca3327cc8549e1c6ba
hernan226 <hernan226@outlook.com>
2019-03-26 19:54:12 -0300

    Add files via upload

---
+++ Guia de ejercicios/ejercicio17/Boligrafo.cs
@@ -0,0 +1,86 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio17
+{
+  class Boligrafo
+  {
+    public const short cantidadTintaMaxima = 100;
+    private ConsoleColor color;
+    private short tinta;
+
+    public Boligrafo(short tinta, ConsoleColor color)
+    {
+      this.color = color;
+      this.tinta = tinta;
+    }
+
+    public ConsoleColor GetColor()
+    {
+      return this.color;
+    }
+
+    public short GetTinta()
+    {
+      return this.tinta;
+    }
+    /// <summary>
+    /// Reutilizando codigo aunque no pidieron este ejercicio
+    /// </summary>
+    /// <param name="gasto">Cantidad de asteriscos a restar en tinta</param>
+    /// <param name="iteracion">Cantidad de asteriscos a pintar</param>
+    /// <returns></returns>
+    private string Pintado(int gasto,int iteracion)
+    {
+      StringBuilder stb = new StringBuilder();
+      for (int i = 0; i < iteracion; i++)
+      {
+        stb.Append("*");
+      }      
+      this.SetTinta((short)gasto);
+      return stb.ToString();
+    }
+
+    public bool Pintar(int gasto, out string dibujo)
+    {      
+      if (gasto<=GetTinta())
+      {
+        dibujo = this.Pintado(gasto, gasto);
+        return true;
+      }
+      else
+      {
+        dibujo = this.Pintado(gasto, tinta);
+        return false;
+      }
+      
+    }
+
+    public void Recargar()
+    {
+      this.tinta = cantidadTintaMaxima;
+    }
+
+    private void SetTinta(short tinta)
+    {
+      if (tinta>0)
+      {
+        if ((tinta + this.tinta) >= cantidadTintaMaxima)
+          this.Recargar();
+        else
+          this.tinta += tinta;
+      }
+      else
+      {
+        if ((this.tinta + tinta) >= 0)
+          this.tinta += tinta;
+        else
+          this.tinta = 0;
+      }
+
+    }
+  }
+}
--- Guia de ejercicios/ejercicio17/Program.cs
+++ Guia de ejercicios/ejercicio17/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +10,23 @@ namespace ejercicio17
   {
     static void Main(string[] args)
     {
+      Boligrafo boliRed = new Boligrafo(50, ConsoleColor.Red);
+      Boligrafo boliBlu = new Boligrafo(100, ConsoleColor.Blue);
+      string retorno;
+
+      bool pudo = boliBlu.Pintar(600, out retorno);
+      if (!pudo)
+        Console.Write("Solo pudo pintar esto: ");
+      Console.ForegroundColor = boliBlu.GetColor();
+      Console.WriteLine(retorno);
+      Console.ReadKey();
+
+      pudo = boliRed.Pintar(40, out retorno);
+      if (!pudo)
+        Console.Write("Solo pudo pintar esto: ");
+      Console.ForegroundColor = boliRed.GetColor();
+      Console.WriteLine(retorno);
+      Console.ReadKey();
     }
   }
 }
