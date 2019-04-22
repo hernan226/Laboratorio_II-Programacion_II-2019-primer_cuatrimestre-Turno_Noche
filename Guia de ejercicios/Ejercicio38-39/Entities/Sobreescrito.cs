b903a1f6f991d1ec597819dd2fda9895a97c10ab
hernan226 <hernan226@outlook.com>
2019-04-22 20:34:44 -0300

    Add files via upload

---
+++ Guia de ejercicios/Ejercicio38-39/Entities/Sobreescrito.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public abstract class Sobreescrito
+    {
+      protected string miAtributo;
+      public abstract string MiPropiedad { get; }
+
+      protected Sobreescrito()
+      {
+        this.miAtributo = "Probar abstractos";
+      }
+
+      public override string ToString()
+      {
+        return "¡Este es mi método ToString sobreescrito!";
+      }
+
+      public override bool Equals(object obj)
+      {
+        return (obj is Sobreescrito);
+      }
+
+      public override int GetHashCode()
+      {
+        return 1142510187;
+      }
+    }
+}
