1382ca12129496c21043aaa6120470d5d7381f9d
hernan226 <hernan226@outlook.com>
2019-06-03 22:00:36 -0300

    Add files via upload

---
+++ Clase21/Consultas.sql
@@ -0,0 +1,12 @@
+/*SELECT * FROM Provincias /*WHERE id = 12*/;
+
+INSERT INTO Provincias (Nombre) VALUES ('Chile');
+
+DELETE FROM Provincias WHERE id = 97;
+
+UPDATE Provincias SET Nombre = 'Cap' where id = 25;
+
+SELECT * FROM Localidades WHERE Nombre LIKE'Z_____';
+
+SELECT * FROM Localidades WHERE Nombre LIKE'ZE%';*/
+
---
+++ Clase21/Ejemplo/Ejemplo/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace Ejemplo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //propiedades del proyecto, (cadena de conexiom) y para acceder al dato
+            //Properties.Settings.Default.SqlString
+
+            string sqlString = Properties.Settings.Default.SqlString;
+
+            SqlConnection sqlConnection;
+            sqlConnection = new SqlConnection(sqlString);
+
+            SqlCommand sqlCommand;
+            sqlCommand = new SqlCommand();
+            sqlCommand.CommandType = System.Data.CommandType.Text;// viene por defecto asi
+            sqlCommand.Connection = sqlConnection;
+
+            sqlCommand.CommandText =
+                "SELECT Nombre FROM Provincias";
+            sqlConnection.Open();
+
+            SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+            List<string> aux = new List<string>();
+            while (sqlDataReader.Read())
+            {
+                 aux.Add(sqlDataReader["Nombre"].ToString());
+            }
+            sqlConnection.Close();
+            /*  crear constructor provincias
+             *  con tostring que muestr nombre
+
+            */
+        }
+    }
+}
